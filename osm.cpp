3d7e84d7479964a7fb9bc69b6f9688fe2373aeed
zahala0017 <zahala.kamari@huji.mail.ac.il>
2020-03-30 14:08:32 +0300

    Initial commit

---
+++ osm.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Admin on 30/03/2020.
+//
+
+#include "osm.h"
+
+double osm_operation_time(unsigned int iterations)
+{
+    return 0;
+}
+
+double osm_function_time(unsigned int iterations)
+{
+    return 0;
+}
+
+double osm_syscall_time(unsigned int iterations)
+{
+    return 0;
+}
+
